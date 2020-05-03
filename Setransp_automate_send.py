bbd404f0be7fd31856d6da5db23d6a6c57e86f02
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2020-05-03 17:11:10 -0300

    Setransp_automate_send builded with CHROME DRIVER

--- Setransp_automate_send.py
+++ Setransp_automate_send.py
@@ -1,3 +1,23 @@
-#Setransp_automate_send
+#Setransp_automate_send builded with CHROME DRIVER
 
 from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:\chromedriver')
+driver.implicitly_wait(10)
+
+def login():
+    #Link: http://www.gemul-aparecida.com.br/login.asp
+    id_login='txtLogin'
+    id_pass='txtSenha'
+    id_btn='#'
+    login='DIEGO.CSF'
+    passw='GMLPALADINO'
+    driver.get('http://www.gemul-aparecida.com.br/login.asp')
+    input_login=driver.find_element_by_id(id_login)
+    input_pass=driver.find_element_by_id(id_pass)
+    btn_login=driver.find_element_by_id(id_btn)
+    input_login.send_keys(login)
+    input_pass.send_keys(passw)
+    btn_login.click()
+
+login()
