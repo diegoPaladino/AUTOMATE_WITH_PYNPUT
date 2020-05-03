f2ede2aecf1ae6fab0dc7280d43e30f52454a08d
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2020-05-03 19:37:47 -0300

    Added some items like links of the tutorials

--- SetranspAutomateEmailSend.py
+++ SetranspAutomateEmailSend.py
@@ -1,8 +1,16 @@
 #SetranspAutomateEmailSend
+#tutoriais:
+#https://www.youtube.com/watch?v=DTnz8wA6wpw    Simulate Key Presses in Python
+#https://www.youtube.com/watch?v=eK7p1e8-6jU    Pynput parte 1 - Controlando o mouse com python
+#https://www.youtube.com/watch?v=MYQnqyVqCDc    Pynput parte 2 - Controlando o teclado com python
+#https://www.youtube.com/watch?v=2BXr9U6ZL8Y    Simulate Mouse Events in Python
+#https://www.youtube.com/watch?v=DTnz8wA6wpw    Simulate Key Presses in Python
+
+
 
 from selenium import webdriver
 from pynput.mouse import Button, Listener, Controller
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, Controller
 import time
 
 # Openning the page on Opera Navigator
@@ -14,19 +22,19 @@ import time
 #Mouse and Keyboard Actions
 
 mouse=Controller()
-keyb=Controller()
-# print(mouse.position)
+keyboard=Controller()
+print(mouse.position)
 # mouse.position=(1124,109)
-# mouse.click(Button.left,1)
-# time.sleep(2)
-# mouse.position=(699, 406)
-# mouse.click(Button.left,1)
-# time.sleep(2)
-# keyb.type("DIEGO.CSF")
-# mouse.position=(699, 467)
+# mouse.click(Button.left)
+# #time.sleep(2)
+# mouse.move(699, 406)
+# mouse.click(Button.left)
+# #time.sleep(2)
+# keyboard.type('DIEGO.CSF')
+# mouse.move(699, 467)
 # mouse.click(Button.left)
 # keyb.type('GMLPALADINO')
 
-keyb.press(Key.cmd)
-keyb.release(Key.cmd)
-keyb.press('c')
+# keyb.press(Key.cmd)
+# keyb.release(Key.cmd)
+# keyb.press('c')
