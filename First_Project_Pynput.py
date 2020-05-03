9605a5c71073ad549054c50b37ec88d29431ef4e
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2020-05-03 14:22:37 -0300

    try of to run the mouse and keyboard join.

--- First_Project_Pynput.py
+++ First_Project_Pynput.py
@@ -1,6 +1,9 @@
 #Lets go!!
 
 from pynput.mouse import Button, Listener
+from pynput.keyboard import Listener, Key
+
+#Mouse actions
 
 def click(x,y, button, pressed):
     print(x,y, button, pressed)
@@ -10,4 +13,16 @@ def move(x,y):
     print(x,y)
 
 with Listener(on_click=click, on_move=move) as listener:
+    listener.join()
+
+#Keyboard actions
+
+def press(key):
+    print(key)
+
+def release(key):
+    if key == Key.shift:
+        return False
+
+with Listener(on_press=press, on_release=release) as listener:
     listener.join()
