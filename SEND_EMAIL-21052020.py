d6a41b389996626e8019ea2419aa36089a29d1e3
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2020-05-23 21:01:18 -0300

    some changes

--- SEND_EMAIL-21052020.py
+++ SEND_EMAIL-21052020.py
@@ -1,8 +1,23 @@
-Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def send_email(subject,msg):
+#Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
+#tutorial link: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
+#SENHA GERADA PELA SEGURANÇA DO GOOGLE: wvqgqcsdymjwsyhp
+
+import smtplib
+
+import config
+
+def send_email(subject,msg):
 	try:
 		server = smtplib.SMTP('smtp.gmail.com:587')
 		server.ehlo()
 		server.starttls()
 		server.login(config.EMAIL_ADDRESS,config.PASSWORD)
+		message = 'Subject:{}\n\n{}'.format(subject, msg)
+		server.sendmail(config.EMAIL_ADDRESS, config.PASSWORD, message)
+		server.quit()
+		print("Sucess: Email sent!")
+	except:
+		print("Email failed to send.")
+
+		
