e8ec089aa9c62dcbedf3c85baed75d21df944152
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2020-05-24 21:03:25 -0300

    last changes of the sucessful program

--- SEND_EMAIL-24052020.py
+++ SEND_EMAIL-24052020.py
@@ -1,5 +1,5 @@
 #link of tutorial: https://www.youtube.com/watch?v=sXjpkcF7rPQ
-
+# #Joining the content of the some previous test (https://www.youtube.com/watch?v=JRCJ6RtE3xU) with this tutorial, was sucess!
 #password='paladino804680'
 
 import smtplib
@@ -22,4 +22,3 @@ print("Login succes")
 server.sendmail(sender_email,rec_email,msg)
 print("Email has sent to",rec_email)
 
-#Wasn't possible to send humbers and special caracters like " : " in the "message"
